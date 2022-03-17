dd93d9044a34234c6c556911e4dbfcb3e6647ea8
kodelan <konlanmikpekoah.km@outlook.com>
2022-03-17 13:29:43 +0000

    card done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,12 @@ import { WeatherserviceService } from 'src/app/service/weatherservice.service';
 export class HomeComponent implements OnInit {
   data : any;
   countryData: any;
+  tempratureDataFeelsLike: number =0;
+  tempratureData: number =0;
+  showFarihient: boolean =false;
+  showCelcius: boolean = true;
+  sybol : String = '°C';
+
   constructor(private geolocationService: GeolocationService, private weatherservice: WeatherserviceService) { }
 
   ngOnInit(): void {
@@ -21,6 +27,8 @@ export class HomeComponent implements OnInit {
       console.log(this.countryData)
       this.weatherservice.getCurrentWeather(value.latitude, value.longitude).subscribe( (value:any) => {
         this.data = value;
+        this.tempratureDataFeelsLike = this.data.main.feels_like;
+        this.tempratureData = this.data.main.temp;
         console.log(this.data);
       }
       )
@@ -30,4 +38,39 @@ export class HomeComponent implements OnInit {
     }
     );
   }
+
+  toogle(value: any){
+    if(value.target.checked){
+     this.tempratureData =  this.getWeatherInFarihient(this.tempratureData);
+     this.tempratureDataFeelsLike =  this.getWeatherInFarihient(this.tempratureDataFeelsLike);
+     this.showFarihient = true;
+     this.showSymbol();
+    }
+    else{
+      this.tempratureData =  this.getWeatherInCelcius(this.tempratureData);
+      this.tempratureDataFeelsLike =  this.getWeatherInCelcius(this.tempratureDataFeelsLike);
+      this.showFarihient = false;
+      this.showSymbol();
+    }
+  }
+
+  getWeatherInFarihient( temperature: number): number{
+    let temInFarihient = temperature * (9/5) + 32;
+    return Number(temInFarihient.toFixed(2));
+  }
+
+  getWeatherInCelcius( temperature: number): number{
+    let tempInCelcius = (temperature - 32) * (5/9);
+    return Number(tempInCelcius.toFixed(2));
+  }
+
+  showSymbol(){
+    if(this.showFarihient == true){
+      this.sybol = '°F';
+    }
+    else{
+      this.sybol = '°C';
+    }
+  }
+
 }
--- src/app/interfaces/weather.ts
+++ src/app/interfaces/weather.ts
@@ -1,2 +1,8 @@
-export interface Weather {
+export class Weather {
+  Temperature: number | undefined;
+  Humidity: number | undefined;
+  WindSpeed: number | undefined;
+  WindDirection: number | undefined;
+  pressure: number | undefined;
+  visibility: number | undefined;
 }
